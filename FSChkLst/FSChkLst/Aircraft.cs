b7b82f4e3ab41a6f5a8c6e06247bab7d2cd97f38
carl <carl.blundon@gmail.com>
2018-09-05 14:53:00 -0230

    Added individual classes

---
+++ FSChkLst/FSChkLst/Aircraft.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSChkLst
+{
+    class Aircraft
+    {
+        private int aircraftID;
+        private string aircraftDescription;
+
+
+    }
+}
